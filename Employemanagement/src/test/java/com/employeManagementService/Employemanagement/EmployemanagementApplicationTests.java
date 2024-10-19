975ef9548a8d9907dba2cc8438237392b643e606
AlbiVarghese <albin.v@vaibhav-vyapaar.com>
2024-10-19 15:06:03 +0530

    initial commit for master

---
+++ Employemanagement/src/test/java/com/employeManagementService/Employemanagement/EmployemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeManagementService.Employemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
